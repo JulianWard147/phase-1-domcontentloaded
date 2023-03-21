889a5435d4be7806f44f8286bbf7ad9e8b841c7f
Julian Ward <wardjulianm@gmail.com>
2023-03-20 18:09:53 -0600

    lab!

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Your code goes here
+
+document.addEventListener("DOMContentLoaded", function() {
+    updateDOM()
+  });
+
+
+function updateDOM(){
+    const targetElement = document.getElementById("text")
+        targetElement.innerHTML = "This is really cool!"
+}
